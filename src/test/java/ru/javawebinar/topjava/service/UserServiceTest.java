13f0a167b1c2dc3e100fcbdfa7356b08209a91c5
Maks Timchenko <m.n.timchenko@gmail.com>
2022-11-08 12:58:35 +1000

    Implement tests by inheritance
    - AbstractServiceTest is base class and has statistic and base CRUD-testing methods.
    - Methods in all classes Abstract/User/Meal - ServiceTest ordered by CRUD.

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -2,16 +2,9 @@ package ru.javawebinar.topjava.service;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.dao.DataAccessException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
@@ -22,14 +15,7 @@ import java.util.List;
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.UserTestData.*;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class UserServiceTest {
+public class UserServiceTest extends AbstractServiceTest {
 
     @Autowired
     private UserService service;
@@ -42,7 +28,7 @@ public class UserServiceTest {
         cacheManager.getCache("users").clear();
     }
 
-    @Test
+    @Override
     public void create() {
         User created = service.create(getNew());
         int newId = created.id();
@@ -52,6 +38,41 @@ public class UserServiceTest {
         USER_MATCHER.assertMatch(service.get(newId), newUser);
     }
 
+    @Override
+    public void get() {
+        User user = service.get(USER_ID);
+        USER_MATCHER.assertMatch(user, UserTestData.user);
+    }
+
+    @Override
+    public void getAll() {
+        List<User> all = service.getAll();
+        USER_MATCHER.assertMatch(all, admin, guest, user);
+    }
+
+    @Override
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND));
+    }
+
+    @Override
+    public void update() {
+        User updated = getUpdated();
+        service.update(updated);
+        USER_MATCHER.assertMatch(service.get(USER_ID), getUpdated());
+    }
+
+    @Override
+    public void delete() {
+        service.delete(USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(USER_ID));
+    }
+
+    @Override
+    public void deleteNotFound() {
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND));
+    }
+
     @Test
     public void duplicateMailCreate() {
         assertThrows(DataAccessException.class, () ->
@@ -59,43 +80,8 @@ public class UserServiceTest {
     }
 
     @Test
-    public void delete() {
-        service.delete(USER_ID);
-        assertThrows(NotFoundException.class, () -> service.get(USER_ID));
-    }
-
-    @Test
-    public void deletedNotFound() {
-        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND));
-    }
-
-    @Test
-    public void get() {
-        User user = service.get(USER_ID);
-        USER_MATCHER.assertMatch(user, UserTestData.user);
-    }
-
-    @Test
-    public void getNotFound() {
-        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND));
-    }
-
-    @Test
     public void getByEmail() {
         User user = service.getByEmail("admin@gmail.com");
         USER_MATCHER.assertMatch(user, admin);
-    }
-
-    @Test
-    public void update() {
-        User updated = getUpdated();
-        service.update(updated);
-        USER_MATCHER.assertMatch(service.get(USER_ID), getUpdated());
-    }
-
-    @Test
-    public void getAll() {
-        List<User> all = service.getAll();
-        USER_MATCHER.assertMatch(all, admin, guest, user);
     }
 }
