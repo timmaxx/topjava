e1834a2e51c4da4378910cbc6a76dc1771eca7c2
Maks Timchenko <m.n.timchenko@gmail.com>
2022-11-23 14:40:53 +1000

    Comment in Abstract...ServiceTest

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -104,8 +104,6 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     public void createWithException() throws Exception {
-        // Поскольку валидация сделана и для jdbc, то этот тест тоже должен проходить.
-        // if (isJdbcProfileSet()) { return; }
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, null, "Description", 300), USER_ID));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID));
--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -152,12 +152,12 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
         List<User> all = service.getAll();
         USER_MATCHER.assertMatch(all, admin, guest, user);
     }
+    // Также, читать это:
+    // https://stackoverflow.com/questions/17814372/jpa-2-0-disable-session-cache-for-unit-tests/58963737#58963737
 */
 
     @Test
     public void createWithException() throws Exception {
-        // Поскольку валидация сделана и для jdbc, то этот тест тоже должен проходить.
-        // if (isJdbcProfileSet()) { return; }
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "  ", "password", Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.USER)));
