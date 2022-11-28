b3421a04c1be16aacd6a150853b3308a64d2d8b7
Maks Timchenko <m.n.timchenko@gmail.com>
2022-11-28 15:02:24 +1000

    master before HW07

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,10 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
@@ -16,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static ru.javawebinar.topjava.UserTestData.*;
-import static ru.javawebinar.topjava.MealTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -32,25 +28,6 @@ class RootControllerTest extends AbstractControllerTest {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
                                 USER_MATCHER.assertMatch(actual, admin, guest, user);
-                            }
-                        }
-                ));
-    }
-
-    @Test
-    void getMeals() throws Exception {
-        perform(get("/meals"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("meals"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals",
-                        new AssertionMatcher<List<MealTo>>() {
-                            @Override
-                            public void assertion(List<MealTo> actual) throws AssertionError {
-                                MEALTO_MATCHER.assertMatch(
-                                        actual,
-                                        MealsUtil.getTos(MealTestData.meals ,SecurityUtil.authUserCaloriesPerDay()));
                             }
                         }
                 ));
