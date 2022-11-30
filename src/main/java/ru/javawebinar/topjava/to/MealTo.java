5f25582666c95a80d0e773a8e4228172a567d944
Maks Timchenko <m.n.timchenko@gmail.com>
2022-11-30 23:29:57 +1000

    HW07 Refactor MealTo

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,30 +1,58 @@
 package ru.javawebinar.topjava.to;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class MealTo {
-    // Сделал пустой конструктор. А следовательно и поля сделал не final.
-    // Это всё для разрешения "Invalid read array from JSON".
-    // Или был другой путь?
-    private /*final*/ Integer id;
 
-    private /*final*/ LocalDateTime dateTime;
+    // Представлены варианты для разрешения "Invalid read array from JSON".
 
-    private /*final*/ String description;
+    // Вариант "Без пустого конструктора, поля можно делать финальными, с аннотациями на полный конструктор @JsonCreator и @JsonProperty"
+    // Видимо более предпочтительный.
+    private final Integer id;
+    private final LocalDateTime dateTime;
+    private final String description;
+    private final int calories;
+    private final boolean excess;
 
-    private /*final*/ int calories;
-
-    private /*final*/ boolean excess;
-
-    public MealTo() {}
-
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+    // Аннотации Jackson: https://nsergey.com/jackson-annotations/
+    @JsonCreator
+    public MealTo(@JsonProperty("id") Integer id,
+                  @JsonProperty("dateTime") LocalDateTime dateTime,
+                  @JsonProperty("description") String description,
+                  @JsonProperty("calories") int calories,
+                  @JsonProperty("excess") boolean excess) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
     }
+
+
+/*    // Вариант "С пустым конструктором, не финальными полями"
+    private Integer id;
+    private LocalDateTime dateTime;
+    private String description;
+    private int calories;
+    private boolean excess;
+
+    public MealTo() {}
+
+    public MealTo(Integer id,
+                  LocalDateTime dateTime,
+                  String description,
+                  int calories,
+                  boolean excess) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+*/
 
     public Integer getId() {
         return id;
