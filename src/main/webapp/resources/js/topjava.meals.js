1004d59e6483190be721965be069dde3ffda5aa5
Maks Timchenko <m.n.timchenko@gmail.com>
2022-12-13 14:41:40 +1000

    HW09 2. Refactor meals.jsp to ajax
    - Row style depends on excess.
    - Time without 'T'.
    - Added i18n.

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -20,11 +20,38 @@ function clearFilter() {
 $(function () {
     makeEditable(
         $("#datatable").DataTable({
+            language: {
+                // Интернациолизация
+                // https://datatables.net/manual/i18n
+
+                // Вариант 1. Прямо здесь по ключевым фразам:
+                // search: "Поиск:",
+
+                // Вариант 2. Безусловный перевод на русский:
+                // https://datatables.net/plug-ins/i18n/Russian.html
+                url: '//cdn.datatables.net/plug-ins/1.13.1/i18n/ru.json'
+                // А как это сделать в общем виде? Т.е. что-бы от настройки консоли браузера зависело?
+            },
+
+            "ajax": {
+                "url": mealAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
                 {
-                    "data": "dateTime"
+                    "data": "dateTime",
+                    "render": function (date, type) {
+                        if (type === "display") {
+                            return date.substring(0, 10) + " " + date.substring(11, 16);
+                        }
+                        return date;
+                    }
+                    /*
+                    , // Была ещё такая попытка:
+                    "render": DataTable.render.datetime('YYYY-MM-DD HH:MI')
+                    */
                 },
                 {
                     "data": "description"
@@ -33,12 +60,14 @@ $(function () {
                     "data": "calories"
                 },
                 {
-                    "defaultContent": "Edit",
-                    "orderable": false
+                    "defaultContent": "",
+                    "orderable": false,
+                    "render": renderEditBtn
                 },
                 {
-                    "defaultContent": "Delete",
-                    "orderable": false
+                    "defaultContent": "",
+                    "orderable": false,
+                    "render": renderDeleteBtn
                 }
             ],
             "order": [
@@ -46,7 +75,14 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            // При формировании <tr> нужно дополнить его
+            // так: <tr data-meal-excess="true">
+            // или так: <tr data-meal-excess="false">
+            "createdRow": function (row, data) {
+                $(row).attr("data-meal-excess", data.excess);
+            }
+
         })
     );
 });
